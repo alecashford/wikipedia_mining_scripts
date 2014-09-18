e57f8793c470958fe4e0243a0a7939f8feeea6f3
Alec Ashford <ashford91@gmail.com>
2014-09-17 17:44:15 -0700

    improved code, made more modular

--- wiki_scrape.py
+++ wiki_scrape.py
@@ -1,38 +1,47 @@
 from bs4 import BeautifulSoup
 import urllib
 import re
+from os import listdir
 
 ### Gets list of all country names ###
 
-# soup = BeautifulSoup(urllib.urlopen('http://en.wikipedia.org/wiki/List_of_sovereign_states'))
+def get_all_names(source_url):
+	soup = BeautifulSoup(urllib.urlopen(source_url))
+	rows = soup.find_all('b')
+	nation_names = [tr.a.text for tr in rows if tr.a is not None and tr.a.text != '^']
+	return [name.encode('ascii','replace') for name in nation_names]
 
-# rows = soup.find_all('b')
-
-# nation_urls = [str(tr.a.text) for tr in rows if tr.a is not None and tr.a.text != '^']
-
-# print nation_urls
+# print get_all_names('http://en.wikipedia.org/wiki/List_of_sovereign_states')
 
 #############################
 
 ### Identifies all nations and downloads them to a folder ###
 
-soup = BeautifulSoup(urllib.urlopen('http://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags'))
+def download_all(source_url, saving_directory):
+	soup = BeautifulSoup(urllib.urlopen(source_url))
+	# Returns a list of the sources all <img> tags with class='thumbborder'
+	img_tags = [str(img['src']) for img in soup.find_all('img', {'class': 'thumbborder'})]
+	# Parses the thumbnail link from the <img> out of the src string
+	thumbnails = [re.match( r'^.+?svg', url).group(0) for url in img_tags if re.match( r'^.+?svg', url) is not None]
+	# Re-formats thumbnail link to link to full-size .svg image
+	full_final_urls = ['http:' + re.sub( r'thumb/', '', url) for url in thumbnails]
+	# Parses useful filename from url, then loops through list and downloads all .svg to a file
+	for image_url in full_final_urls:
+		filename = image_url.split('/')[-1][8:].lower()
+		if filename[:4] == 'the_':
+			filename = filename[4:]
+		# Some final, optional  regex to clean up file names
+		# filename = re.sub( r'%2c', '', filename)
+		urllib.urlretrieve(image_url, saving_directory + filename)
 
-# Returns a list of the sources all <img> tags with class='thumbborder'
-img_tags = [str(img['src']) for img in soup.find_all('img', {'class': 'thumbborder'})]
-
-# Parses the thumbnail link from the <img> out of the src string
-thumbnails = [re.match( r'^.+?svg', url).group(0) for url in img_tags]
-
-# Re-formats thumbnail link to link to full-size .svg image
-full_final_urls = ['http:' + re.sub( r'thumb/', '', url) for url in thumbnails]
-
-# Parses useful filename from url, then loops through list and downloads all .svg to a file
-for image_url in full_final_urls:
-	filename = image_url.split('/')[-1][8:].lower()
-	if filename[:4] == 'the_':
-		filename = filename[4:]
-	urllib.urlretrieve(image_url, '/Users/aashford/desktop/all_flags/' + filename)
+# download_all('http://en.wikipedia.org/wiki/Flags_of_cities_of_the_United_States', '/Users/aashford/desktop/city_flags/')
 
 #############################
+
+### Identifies all nations and downloads them to a folder ###
+def get_file_names(source_directory):
+	return listdir(source_directory)
+
+# print get_file_names('/Users/aashford/Desktop/city_flags')
+
 
